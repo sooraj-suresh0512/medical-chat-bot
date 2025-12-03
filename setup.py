7b6632f19252ef3597fd86e8150ebf72b180aa79
sooraj-suresh0512 <soorajsuresh94539@gmail.com>
2025-12-03 19:36:55 +0530

    Added entire project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chat-bot",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Sooraj Suresh",
+    author_email="mephistosir92@gmail.com",
+    install_requires=[]
+
+)
